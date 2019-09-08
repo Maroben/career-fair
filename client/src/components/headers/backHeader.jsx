3d6e8fcc9f71aee79a998bd20c972bd2ec0612e0
Dijan Helbling <maroben@daishen.ch>
2019-09-08 16:48:22 +0200

    Icon

--- client/src/components/headers/backHeader.jsx
+++ client/src/components/headers/backHeader.jsx
@@ -3,11 +3,7 @@ import PropTypes from "prop-types"
 import { withStyles } from "@material-ui/core/styles"
 import { Link } from "react-router-dom"
 
-import AppBar from "@material-ui/core/AppBar"
-import Toolbar from "@material-ui/core/Toolbar"
-import Typography from "@material-ui/core/Typography"
-import IconButton from "@material-ui/core/IconButton"
-import ChevronLeft from "@material-ui/icons/ChevronLeft"
+import { AppBar, Toolbar, Typography, IconButton, Icon } from "@material-ui/core"
 
 const styles = (theme) => ({
 	root: {
@@ -16,9 +12,6 @@ const styles = (theme) => ({
 	title: {
 		display: "block",
 		width: "100%"
-	},
-	backButton: {
-		marginRight: theme.spacing(2)
 	}
 })
 
@@ -26,14 +19,8 @@ const BackHeader = ({ classes, title }) => {
 	return (
 		<AppBar position="static" className={classes.root}>
 			<Toolbar>
-				<IconButton
-					edge="start"
-					className={classes.backButton}
-					color="inherit"
-					component={Link}
-					to={"/companies"}
-				>
-					<ChevronLeft />
+				<IconButton edge="start" color="inherit" component={Link} to={"/companies"}>
+					<Icon className={"fas fa-chevron-left"} />
 				</IconButton>
 				<Typography className={classes.title} variant="h6" color="inherit" noWrap>
 					{title}
