214ce4409280f2b891be64c63f6709245971c2e8
Maroben <maroben@daishen.ch>
2019-12-06 14:27:14 +0000

    db tests

--- __tests__/persistence/database.test.ts
+++ __tests__/persistence/database.test.ts
@@ -21,14 +21,48 @@ describe("Company Model", () => {
 
     it("CRUD Company", async () => {
         const db = new Database<ICompany>(Company)
-        const company: ICompany = new Company({
+
+        expect(async () => await db.get("fakeid")).rejects
+
+        const company1: ICompany = new Company({
             name: "Name1",
             info: "Info1",
             description: "Description1",
             location: "Location1"
         })
+        const company2: ICompany = new Company({
+            name: "Name2",
+            info: "Info2",
+            description: "Description2",
+            location: "Location2"
+        })
 
-        const received = await db.post(company)
-        expect(_.pick(received, properties)).toMatchSnapshot()
+        const received1 = await db.post(company1)
+        const received2 = await db.post(company2)
+        expect(_.pick(received1, properties)).toMatchSnapshot()
+        expect(_.pick(received2, properties)).toMatchSnapshot()
+
+        const received3 = await db.getAll()
+        expect(received3).toHaveLength(2)
+
+        const received4 = await db.get(received2._id)
+
+        expect(_.pick(received2, ["_id", ...properties])).toMatchObject(
+            _.pick(received4, ["_id", ...properties])
+        )
+
+        const company5: ICompany = new Company({
+            name: "Name5",
+            info: "Info5",
+            description: "Description5",
+            location: "Location5"
+        })
+
+        const received5 = await db.put(company5, received1._id)
+        expect(_.pick(received5, properties)).toMatchSnapshot()
+
+        const received6 = await db.delete(received1._id)
+        expect(received6).toMatchObject(received5)
+        expect(async () => await db.get(received1._id)).rejects
     })
 })
--- src/persistence/database.ts
+++ src/persistence/database.ts
@@ -3,65 +3,69 @@ import mongoose from "mongoose"
 export default class Database<T extends mongoose.Document> {
     constructor(private readonly model: mongoose.Model<T>) {}
 
-    public async getAll() {
+    public async getAll(): Promise<T[]> {
         return await this.model
             .find()
-            .then((document) => {
-                return document
+            .then((documents) => {
+                return documents
             })
             .catch((err) => {
                 throw new Error(`No Documents found!`)
             })
     }
 
-    public async get(_id: string) {
+    public async get(_id: string): Promise<T> {
         return await this.model
             .findOne({ _id })
             .then((document) => {
                 if (document) {
-                    return document
-                } else {
-                    throw Error
+                    return document as T
                 }
+                throw Error
             })
             .catch((err) => {
                 throw new Error(`Document with ID: ${_id} not found!`)
             })
     }
 
-    public async post(document: T) {
+    public async post(document: T): Promise<T> {
         return await this.model
             .create(document)
             .then((document) => {
-                return document
+                return document as T
             })
             .catch((err) => {
                 throw new Error(`Failed to create Document!`)
             })
     }
 
-    public async put(document: T, _id: string) {
+    public async put(document: T, _id: string): Promise<T> {
+        document._id = _id // overwrite the generated id
         return await this.model
-            .findOneAndUpdate({ _id }, document, { new: true })
+            .findOneAndUpdate({ _id: _id }, document, { new: true })
             .then((document) => {
-                return document
+                if (document) {
+                    return document as T
+                }
+                throw Error
             })
             .catch((err) => {
+                console.error(err)
                 throw new Error(`Failed to update Document with ID: ${_id}!`)
             })
     }
 
-    public async delete(_id: string) {
+    public async delete(_id: string): Promise<T> {
         return await this.model
             .findOneAndDelete({ _id })
             .then((document) => {
                 if (document) {
-                    return document
-                } else {
-                    throw Error
+                    return document as T
                 }
+                throw Error
             })
             .catch((err) => {
+                console.error(err)
                 throw new Error(`Failed to delete Document with ID: ${_id}!`)
             })
     }
